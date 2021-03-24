fd89ea46ce821e2cfb89032a03229cc46d24e97c
sasha <ortonwind96@gmail.com>
2021-03-24 15:58:03 +0200

    Have working version of parser that parse <title, link, price, city and time> at olx.ua website and create .csv file with parsed data

--- practise_2/parser.py
+++ practise_2/parser.py
@@ -1,17 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
-from requests.models import Response
+import csv
 
-URL = "https://www.olx.ua/nedvizhimost/kvartiry-komnaty/kiev/"
+URL = "https://www.olx.ua/transport/legkovye-avtomobili/acura/kiev/"
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0',
            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
            }
-HOST = 'https://www.olx.ua'
+FILE = 'olx_parse.csv'
 
 
 def get_html(url, params=None):
     r = requests.get(url, headers=HEADERS, params=params)
     return r
+
+
+def get_pages_count(html):
+    # function is scrolling pages
+    soup = BeautifulSoup(html, 'html.parser')
+    pagination = soup.find_all('span', class_='item fleft')
+    if pagination:
+        return int(pagination[-1].get_text())
+    else:
+        return 1
 
 
 def get_content(html):
@@ -22,16 +32,31 @@ def get_content(html):
         olx.append({
             'title': item.find('div', class_='space rel').get_text(strip=True),
             'link': item.find('a', class_='marginright5').get('href'),
-            'Price': item.find('p', class_='price').get_text(strip=True),
-            'City and time': item.find('td', class_='bottom-cell').get_text(strip=True),
+            'price': item.find('p', class_='price').get_text(strip=True),
+            'city and time': item.find('td', class_='bottom-cell').get_text(strip=True),
         })
     return olx
+
+
+def save_file(items, path):
+    with open(path, 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow(['title', 'link', 'price', 'city and time'])
+        for item in items:
+            writer.writerow([item['title'], item['link'], item['price'], item['city and time']])
 
 
 def parse():
     html = get_html(URL)
     if html.status_code == 200:
-        olx = get_content(html.text)
+        olx = []
+        pages_count = get_pages_count(html.text)
+        for page in range(1, pages_count+1):
+            print(f'Parcing...Page {page} out of {pages_count}')
+            html = get_html(URL, params={'page': page})
+            olx.extend(get_content(html.text))
+        save_file(olx, FILE)
+        print(f'Got {len(olx)} matches')
     else:
         print('Error')
 
