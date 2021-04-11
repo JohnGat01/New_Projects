2ab3ec28b40d53a1b9dd28b9999314b2f005d563
sasha <ortonwind96@gmail.com>
2021-04-11 14:37:42 +0300

    Done snowfall with 20 snowy

--- practise_7/01_winter.py
+++ practise_7/01_winter.py
@@ -1,31 +1,30 @@
 import simple_draw as sd
 from simple_draw import *
-from random import randint
 
 sd.resolution = (800, 800)
 
-x = [100, 700, 200, 150, 550, 340, 544, 656, 76, 564, 77, 43, 656, 432, 123, 545]
-y = 700
+snow = []
+for i in range(20):
+    x = randint(100, 500)
+    y = 700
+    snow.append([x, y])
 
-while True:
-    sd.clear_screen()
-    point = sd.get_point(x[0], y)
-    sd.snowflake(center=point, length=50, color=COLOR_WHITE, factor_a=0.6, factor_b=0.35, factor_c=60)
-    y -= randint(4, 15)
-    if y < -50:
-        break
-    x[0] += randint(5, 20)
+def snowfall(c):
+    while True:
+        for c in snow:
+            point = sd.get_point(c[0], c[1])
+            sd.snowflake(center=point, length=30, color=COLOR_WHITE, factor_a=0.6, factor_b=0.35,
+                         factor_c=60)
+            c[0] += randint(-10, 20)
+            c[1] -= randint(2, 30)
+            if c[1] < -50:
+                break
+        sd.sleep(0.2)
+        sd.clear_screen()
+        if sd.user_want_exit():
+            break
 
-#     point_2 = sd.get_point(x[1], y[1])
-#     sd.snowflake(center=point_2, length=30, color=COLOR_WHITE, factor_a=0.6, factor_b=0.35, factor_c=60)
-#     y[1] -= randint(2, 10)
-#     if y[1] < -50:
-#         break
-#     x[1] -= randint(3, 12)
-#     sd.sleep(0.1)
-#
-#     if sd.user_want_exit():
-#         break
 
+snowfall(c=snow)
 
 sd.pause()
