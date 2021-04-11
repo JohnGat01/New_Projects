b6ace70d37834fd110fd69715964a8092ba059df
sasha <ortonwind96@gmail.com>
2021-04-11 14:49:25 +0300

    Done rainbow, change unpack <rainbow_colors> with <for>

--- practise_6/06_rainbow.py
+++ practise_6/06_rainbow.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # (цикл for)
 import simple_draw as sd
-from random import choice
 
 sd.resolution = (1200, 600)
 rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
@@ -9,15 +8,10 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
 radius = 500
 
 
-def random_color():
-    colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
-              sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
-    return choice(colors)
-
 
 for i in range(7):
     point = sd.get_point(500, 0)
-    sd.circle(point, radius, random_color(), 20)
+    sd.circle(point, radius, rainbow_colors[i], 20)
     radius += 20
 
 sd.pause()
