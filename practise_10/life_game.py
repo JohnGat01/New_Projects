54c13929079c02a24405e93df86c7b0719c3f098
sasha <ortonwind96@gmail.com>
2021-04-23 19:50:04 +0300

    Done new functionality of game made new class Woman, in class Man done function of playing poker, add metric - happiness

--- practise_10/life_game.py
+++ practise_10/life_game.py
@@ -2,6 +2,19 @@ from random import randint
 from termcolor import cprint
 
 
+class House:
+
+    def __init__(self):
+        self.food = 50
+        self.money = 100
+        self.cat_food = 50
+        self.purity = 100
+
+    def __str__(self):
+        return 'In fridge we have {} food and {} cat food, purity in house is {}, in nightstand we have {}$'.format(
+            self.food, self.cat_food, self.purity, self.money)
+
+
 class Man:
 
     def __init__(self, name):
@@ -9,15 +22,32 @@ class Man:
         self.fullness = 100
         self.IQ = 80
         self.house = None
+        self.happiness = 100
 
     def __str__(self):
-        return "I'm {}, my fullness is {},my IQ is {}".format(self.name, self.fullness, self.IQ)
+        return "I'm {}, my fullness is {},my IQ is {}, my happiness - {}".format(
+            self.name, self.fullness, self.IQ, self.happiness)
+
+    def go_into_the_house(self, house):
+        self.house = house
+        self.fullness -= 10
+        print('{} go into house!!!'.format(self.name))
 
     def eat(self):
-        if self.house.food >= 10:
+        if self.house.food >= 30:
             print("{} ate".format(self.name))
-            self.fullness += 10
-            self.house.food -= 10
+            if self.happiness <= 90:
+                self.happiness += 10
+
+            if self.fullness < 50:
+                self.house.food -= 30
+                self.fullness += 30
+            elif self.fullness < 30:
+                self.house.food -= 20
+                self.fullness += 20
+            else:
+                self.fullness += 10
+                self.house.food -= 10
         else:
             print("I don't have food...")
 
@@ -32,26 +62,99 @@ class Man:
         elif self.IQ > 100:
             self.house.money += 150
         elif self.IQ < 100:
-            self.house.money += 75
-        self.fullness -= 20
+            self.house.money += 100
         if self.IQ < 200:
             self.IQ += 1
-
-    def buy_food(self):
-        print("{} go to shop".format(self.name))
-        self.house.money -= 50
-        self.house.food += 30
-
-    def buy_cat_food(self):
-        print("{} bought cat food".format(self.name))
-        self.house.money -= 50
-        self.house.cat_food += 50
+        self.fullness -= 10
+        self.happiness -= 10
 
     def read_books(self):
         print("{} reading books".format(self.name))
         self.fullness -= 10
         if self.IQ < 199:
             self.IQ += 2
+        if self.happiness <= 95:
+            self.happiness += 5
+
+    def play_poker(self):
+        dice = randint(1, 100)
+        if dice < 33:
+            self.house.money -= 50
+            self.happiness -= 10
+            print(f'{self.name} played poker and lost money... he is sad')
+        elif 33 > dice < 66:
+            self.house.money += 50
+            if self.happiness <= 95:
+                self.happiness += 5
+            print(f'{self.name} played poker and win some money! He is quite happy')
+        else:
+            if self.happiness <= 95:
+                self.happiness += 15
+            self.house.money += 250
+            print(f'{self.name} played poker and won tournament!!! He feels like Elon Musk!')
+
+    def act(self):
+        dice = randint(1, 2)
+        if self.fullness <= 50:
+            self.eat()
+        elif self.house.money <= 100:
+            self.go_work()
+        elif dice == 1:
+            self.read_books()
+        else:
+            self.play_poker()
+
+        if self.house.purity <= -20:
+            self.happiness -= 10
+
+
+class Women:
+
+    charity_foundation = 0
+
+    def __init__(self, name):
+        self.name = name
+        self.fullness = 100
+        self.IQ = 80
+        self.house = None
+        self.happiness = 100
+
+    def __str__(self):
+        return "I'm {}, my fullness is {},my IQ is {}, my happiness - {}".format(
+            self.name, self.fullness, self.IQ, self.happiness)
+
+    def go_into_the_house(self, house):
+        self.house = house
+        self.fullness -= 10
+        print('{} go into house!!!'.format(self.name))
+
+    def eat(self):
+        if self.house.food >= 30:
+            print("{} ate".format(self.name))
+            if self.happiness <= 90:
+                self.happiness += 10
+
+            if self.fullness < 50:
+                self.house.food -= 30
+                self.fullness += 30
+            elif self.fullness < 30:
+                self.house.food -= 20
+                self.fullness += 20
+            else:
+                self.fullness += 10
+                self.house.food -= 10
+        else:
+            print("I don't have food...")
+
+    def house_food(self):
+        print("{} go to shop".format(self.name))
+        self.house.money -= 50
+        self.house.food += 30
+        if self.house.cat_food <= 100:
+            print(f"{self.name} bought cat food")
+            self.house.money -= 50
+            self.house.cat_food += 50
+        self.happiness -= 10
 
     def clean_house(self):
         print('{} cleaned the house'.format(self.name))
@@ -64,44 +167,42 @@ class Man:
         else:
             self.fullness -= 20
             self.house.purity += 100
+        self.happiness -= 5
 
-    def go_into_the_house(self, house):
-        self.house = house
+    def save_earth(self):
+        charity_fee = 350
+        self.house.money -= charity_fee
+        print(f'{self.name} is saving the world! She gave to charity {charity_fee}$')
+        Women.charity_foundation += charity_fee
+        if self.happiness <= 70:
+            self.happiness += 30
+
+    def read_books(self):
+        print("{} reading books".format(self.name))
         self.fullness -= 10
-        print('{} go into house!!!'.format(self.name))
+        if self.IQ < 199:
+            self.IQ += 2
 
-    def act_my_wife(self):
-        dice = randint(1, 2)
+    def act(self):
+
         if self.fullness <= 50:
             self.eat()
 
         elif self.house.food <= 50:
-            self.buy_food()
-            if self.house.cat_food <= 50:
-                self.buy_cat_food()
+            self.house_food()
 
         elif self.house.purity <= 0:
             self.clean_house()
 
+        elif self.house.money >= 1000:
+            self.save_earth()
+
         else:
-            if dice == 1:
-                self.go_work()
-            if dice == 2:
-                self.read_books()
-
-    def act_me(self):
-        dice = randint(1, 4)
-        if self.fullness <= 50:
-            self.eat()
-
-        elif self.house.money <= 100:
-            self.go_work()
-
-        elif dice == 1:
             self.read_books()
 
-        else:
-            self.go_work()
+        if self.house.purity <= -20:
+            self.happiness -= 10
+
 
 class Cat:
 
@@ -147,35 +248,21 @@ class Cat:
             self.sleep()
 
 
-class House:
-
-    def __init__(self):
-        self.food = 50
-        self.money = 100
-        self.cat_food = 50
-        self.purity = 100
-
-    def __str__(self):
-        return 'In fridge we have {} food and {} cat food, purity in house is {}, in bank we have {}$'.format(
-            self.food, self.cat_food, self.purity,self.money)
-
-
-my_wife = Man(name="Oksana")
 me = Man(name="Sasha")
+my_wife = Women(name="Oksana")
 cat_1 = Cat(name="Shashlichok")
 cat_2 = Cat(name='Kukuryza')
 cat_3 = Cat(name='Moloko')
 
-
-
 our_sweet_home = House()
+
 me.go_into_the_house(house=our_sweet_home)
 my_wife.go_into_the_house(house=our_sweet_home)
 cat_1.go_into_the_house(house=our_sweet_home)
 cat_2.go_into_the_house(house=our_sweet_home)
 cat_3.go_into_the_house(house=our_sweet_home)
 
-for day in range(366 * 3):
+for day in range(365):
     if my_wife.fullness <= 0:
         print("{} dead on {} day of life...".format(my_wife.name, day))
         break
@@ -184,13 +271,17 @@ for day in range(366 * 3):
         break
     if cat_1.fullness <= 0:
         print("{} is dead...".format(cat_1.name, day))
+        break
     if cat_2.fullness <= 0:
         print("{} is dead...".format(cat_1.name, day))
+        break
     if cat_3.fullness <= 0:
         print("{} is dead...".format(cat_1.name, day))
-    cprint('=============== Day {} ==============='.format(day), color='yellow')
-    me.act_me()
-    my_wife.act_my_wife()
+        break
+
+    cprint(f'=============== Day {day} ===============', color='yellow')
+    me.act()
+    my_wife.act()
     cat_1.act()
     cat_2.act()
     cat_3.act()
@@ -200,3 +291,5 @@ for day in range(366 * 3):
     cprint(cat_2, color='yellow')
     cprint(cat_3, color='yellow')
     print(our_sweet_home)
+
+print(f'{my_wife.name} charity to save the world is {Women.charity_foundation}$')
